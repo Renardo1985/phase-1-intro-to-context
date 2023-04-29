1e6921f40e9b5f6d25c2e255093768648b07467e
Renardo Williams <rennwill@gmail.com>
2023-04-28 21:07:37 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-// Your code here
+function createEmployeeRecord(eArray){
+   
+    const employee = {}
+    employee.firstName = eArray[0];
+    employee.familyName = eArray[1];
+    employee.title = eArray[2];
+    employee.payPerHour = eArray[3];
+    employee.timeInEvents = [];
+    employee.timeOutEvents = [];
+    return employee;
+}
+
+function createEmployeeRecords(arrs){
+const employeeArray = [];
+    for (let i = 0; i < arrs.length; i++){
+        employeeArray[i] = createEmployeeRecord(arrs[i])
+    }
+    return employeeArray;
+}
+
+function createTimeInEvent(eObj,timeString){
+    const punch = {};
+    punch.type = "TimeIn";
+    punch.hour = parseInt(timeString.split(' ')[1]);
+    punch.date = timeString.split(' ')[0];
+    eObj.timeInEvents.push(punch)
+    return eObj;
+}
+
+function createTimeOutEvent(eObj,timeString){
+    const punch = {};
+    punch.type = "TimeOut";
+    punch.hour = parseInt(timeString.split(' ')[1]);
+    punch.date = timeString.split(' ')[0];
+    eObj.timeOutEvents.push(punch)
+    return eObj;
+}
+
+function hoursWorkedOnDate(eObj,date){
+    const startTime = eObj.timeInEvents.find(time =>time.date===date).hour/100;
+    const endTime = eObj.timeOutEvents.find(time =>time.date===date).hour/100;
+
+    return endTime - startTime;
+}
+function wagesEarnedOnDate(eObj, date){
+    const hours = hoursWorkedOnDate(eObj,date);
+    const pay = hours*eObj.payPerHour;
+    return pay;
+}
+function allWagesFor (eObj) 
+{
+    const dates = eObj.timeInEvents.map(function(e){return e.date});    
+    const pay = dates.reduce(function (memo, d) {return memo + wagesEarnedOnDate(eObj, d)}, 0)
+    return pay;
+}
+
+
+ function calculatePayroll(eObj){
+    let payroll = eObj.reduce(function(memo,eObj){
+       return memo+allWagesFor(eObj)
+   },0)
+   return payroll;
+ }
